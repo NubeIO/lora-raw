0fbddc0ca446ef8f0146787edbc9035ab85d66a1
NubeDev <aidan@envrioservices.com.au>
2020-11-19 08:16:45 +1100

    git rm -r --cached .

---
+++ tests/arduino/example/example.ino
@@ -0,0 +1,33 @@
+void setup() {
+Serial.begin(9600);//sets the baud rate
+}
+
+void loop() {
+
+for (int i = 0; i <= 4; i++) {
+    Serial.println("AAB296C4E5094228BA0000EC0000009A2D64");
+   delay(10);
+    }
+
+delay(10000);
+for (int i = 0; i <= 1; i++) {
+    Serial.println("ADB232C688082328C32B00EBED491FD8415F");
+   delay(10);
+    }
+
+delay(10000);
+for (int i = 0; i <= 4; i++) {
+    Serial.println("CCB22E0BE8071D28C48E00EBA04F2FE03C64");
+   delay(10);
+    }
+
+delay(10000);
+
+for (int i = 0; i <= 10; i++) {
+    Serial.println("9BB2A166E9081C28373900EA21D0CEE03C61");
+   delay(10);
+    }
+
+delay(10000);
+
+}
