f1a955c43c30ddac59cc73bddf10001842e198dd
zero88 <sontt246@gmail.com>
2020-12-28 18:30:15 +0700

    - Change app setting file from ini to json

--- tests/setting_dump.py
+++ tests/setting_dump.py
@@ -1,27 +1,39 @@
-import json
-from configparser import ConfigParser
-from io import StringIO
-
-from src import MqttSetting, AppSetting, SerialSetting
-
-
-def dump():
-    _parser = ConfigParser()
-    _parser[SerialSetting.KEY] = SerialSetting().__dict__
-    _parser[MqttSetting.KEY] = MqttSetting().__dict__
-
-    with StringIO() as ss:
-        _parser.write(ss)
-        ss.seek(0)
-        return ss.read()
-
+from src import AppSetting, SerialSetting
 
 if __name__ == '__main__':
-    setting = dump()
-    print(setting)
-    parser = ConfigParser()
-    parser.read_string(setting)
-    app_setting = AppSetting()._reload(parser)
+    setting = '''
+    {
+      "mqtt": {
+        "enabled": true,
+        "name": "lora-raw-mqtt",
+        "host": "0.0.0.0",
+        "port": 1883,
+        "keepalive": 60,
+        "qos": 1,
+        "retain": false,
+        "attempt_reconnect_on_unavailable": true,
+        "attempt_reconnect_secs": 5,
+        "topic": "lora_raw",
+        "debug": true,
+        "debug_topic": "debug_lora_raw"
+      },
+      "serial": {
+        "enabled": true,
+        "name": "lora-raw-network",
+        "port": "/dev/ttyUSB0",
+        "baud_rate": 9600,
+        "stop_bits": 1,
+        "parity": "N",
+        "byte_size": 8,
+        "timeout": 5
+      }
+    }
+    '''
+    app_setting = AppSetting().reload(setting, None, is_json_str=True)
     print(type(app_setting.mqtt))
     print(type(app_setting.serial))
-    print(json.dumps(app_setting.mqtt.__dict__))
+    print(type(app_setting.mqtt.enabled))
+    print('-' * 30)
+    print(SerialSetting().serialize())
+    print('-' * 30)
+    print(AppSetting().serialize())
