ad8d659add78a5898a02a314e0385b934d28de3a
RaiBnod <binodrayee@gmail.com>
2021-01-17 10:03:07 +0545

    Add MqttStreamHandler for streaming logs to MQTT

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,4 @@ from .background import FlaskThread
 from .server import GunicornFlaskApplication
 from .setting import AppSetting, SerialSetting, MqttSetting
 from .utils.color_formatter import ColorFormatter
+from .utils.mqtt_stream_handler import MqttStreamHandler
